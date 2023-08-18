9868db1399cd35637d6d9c0c508aee31103061a8
Ali <alidrl26@gmail.com>
2023-08-18 11:24:01 +0200

    sales edit mode on table

--- src/components/SaleTable.jsx
+++ src/components/SaleTable.jsx
@@ -1,89 +1,191 @@
 /* eslint-disable react/jsx-key */
 import Box from '@mui/material/Box';
-import { DataGrid, GridActionsCellItem, GridToolbar } from '@mui/x-data-grid';
+import { DataGrid, GridActionsCellItem, GridRowEditStopReasons, GridRowModes, GridToolbar } from '@mui/x-data-grid';
 import { useSelector } from 'react-redux';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import { btnStyle } from '../styles/globalStyles';
 import useStockCall from '../hooks/useStockCall';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import EditIcon from '@mui/icons-material/Edit';
+import { useState } from 'react';
+
 
 
 
 
 export default function SaleTable() {
 
-  const { sales } = useSelector(state => state.stock)
-  const { deleteStockData } = useStockCall()
+    const { sales, brands, products } = useSelector(state => state.stock)
+    const { deleteStockData, updateStockData } = useStockCall()
+    const [rowModesModel, setRowModesModel] = useState({});
 
-  const columns = [
-    { field: 'createds', headerName: 'Date', headerAlign: "center", flex: 1, align: "center", },
-    {
-      field: 'brand',
-      headerName: 'Brand',
-      headerAlign: "center",
-      align: "center",
-      flex: 2
-    },
-    {
-      field: 'product',
-      headerName: 'Product',
-      headerAlign: "center",
-      align: "center",
-      flex: 2
-    },
-    {
-      field: 'quantity',
-      headerName: 'Quantity',
-      sortable: false,
-      type: 'number',
-      headerAlign: "center",
-      align: "center",
-      flex: 1,
+    const handleRowEditStop = (params, event) => {
+        if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+            event.defaultMuiPrevented = true;
+        }
+    };
 
-    },
-    {
-      field: 'price',
-      headerName: 'Price',
-      headerAlign: "center",
-      // type: 'number',
-      align: "center",
-      flex: 1
-    },
-    {
-      field: 'price_total',
-      headerName: 'Amount',
-      headerAlign: "center",
-      // type: 'number',
-      align: "center",
-      flex: 1
-    },
-    {
-      field: 'actions',
-      headerName: 'Actions',
-      headerAlign: "center",
-      type: "actions",
-      align: "center",
-      flex: 1,
-      getActions: (params) => [
-        < GridActionsCellItem
-          icon={< DeleteForeverIcon />}
-          label='Delete'
-          sx={btnStyle}
-          onClick={() => deleteStockData("purchases", params.id)} />
-      ],
-    },
-  ];
+    const handleEditClick = (id) => () => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+    };
+
+    const handleSaveClick = (id) => () => {
+
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    };
+
+    const handleCancelClick = (id) => () => {
+        setRowModesModel({
+            ...rowModesModel,
+            [id]: { mode: GridRowModes.View, ignoreModifications: true },
+        });
+
+    };
+
+    const processRowUpdate = (newRow) => {
+        let updatedData = {
+            brand_id: newRow.brand_id,
+            product_id: newRow.product_id,
+            quantity: newRow.quantity,
+            price: newRow.price
+        }
+        updateStockData('sales', newRow.id ,updatedData)
+        return newRow;
+    };
+
+    const handleRowModesModelChange = (newRowModesModel) => {
+        setRowModesModel(newRowModesModel);
+    };
+
+    let brandNames = []
+    let productNames = []
+
+    brands.forEach(element => {
+        brandNames.push(element.name)
+    });
+    products.forEach(element => {
+        productNames.push(element.name)
+    });
+    const columns = [
+        { field: 'createds', headerName: 'Date', headerAlign: "center", flex: 1, align: "center", },
+        {
+            field: 'brand',
+            headerName: 'Brand',
+            headerAlign: "center",
+            align: "center",
+            editable: true,
+            type: 'singleSelect',
+            valueOptions: brandNames,
+            flex: 2
+
+        },
+        {
+            field: 'product',
+            headerName: 'Product',
+            headerAlign: "center",
+            align: "center",
+            editable: true,
+            type: 'singleSelect',
+            valueOptions: productNames,
+            flex: 2
+        },
+        {
+            field: 'quantity',
+            headerName: 'Quantity',
+            sortable: false,
+            type: 'number',
+            headerAlign: "center",
+            align: "center",
+            editable: true,
+            flex: 1,
+
+        },
+        {
+            field: 'price',
+            headerName: 'Price',
+            headerAlign: "center",
+            type: 'number',
+            align: "center",
+            editable: true,
+            flex: 1
+        },
+        {
+            field: 'price_total',
+            headerName: 'Amount',
+            headerAlign: "center",
+            type: 'number',
+            align: "center",
+            flex: 1
+        },
+        {
+            field: 'actions',
+            headerName: 'Actions',
+            headerAlign: "center",
+            type: "actions",
+            align: "center",
+            flex: 1,
+            getActions: ({ id }) => {
+                const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+                if (isInEditMode) {
+                    return [
+                        <GridActionsCellItem
+                            icon={<SaveIcon />}
+                            label="Save"
+                            sx={{
+                                color: 'primary.main',
+                            }}
+                            onClick={handleSaveClick(id)}
+                        />,
+                        <GridActionsCellItem
+                            icon={<CancelIcon />}
+                            label="Cancel"
+                            className="textPrimary"
+                            onClick={handleCancelClick(id)}
+                            color="inherit"
+                        />,
+                    ]
+                }
+
+                return [
+                    <GridActionsCellItem
+                        icon={<EditIcon />}
+                        label="Edit"
+                        className="textPrimary"
+                        onClick={handleEditClick(id)}
+                        color="inherit"
+                    />,
+                    < GridActionsCellItem
+                        icon={< DeleteForeverIcon />}
+                        label='Delete'
+                        sx={btnStyle}
+                        onClick={() => deleteStockData("sales", id)} />
+                ]
 
 
-  return (
-    <Box sx={{ width: '100%', mt: 5 }}>
-      <DataGrid
-        rows={sales}
-        columns={columns}
-        // pageSize={10}
-        // pageSizeOptions={[10, 30, 50]}
-        disableRowSelectionOnClick
-        slots={{ toolbar: GridToolbar }}
-      />
-    </Box>
-  );
+
+            }
+
+
+        },
+    ];
+
+
+    return (
+        <Box sx={{ width: '100%', mt: 5 }}>
+            <DataGrid
+                rows={sales}
+                columns={columns}
+                editMode="row"
+                rowModesModel={rowModesModel}
+                onRowModesModelChange={handleRowModesModelChange}
+                onRowEditStop={handleRowEditStop}
+                processRowUpdate={processRowUpdate}
+                pageSize={10}
+                pageSizeOptions={[10, 30, 50]}
+                disableRowSelectionOnClick
+                slots={{ toolbar: GridToolbar }}
+            />
+        </Box>
+    );
 }
--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -28,7 +28,6 @@ const Sales = () => {
 
       <SaleTable/>
 
-
       <SaleModal open={open} setOpen={setOpen}  />
 
     </div>
