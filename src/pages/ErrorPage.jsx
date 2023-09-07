ac7f0992612644f9efe1c3dd3c408379867cf01c
Ali <alidrl26@gmail.com>
2023-09-07 13:46:24 +0200

    img added

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,12 @@
+import { Box } from "@mui/material"
+import notfoundimg from "../assets/errorimg.jpg"
+
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <Box sx={{display: "flex", justifyContent: 'center', height: "80vh", alignItems: 'center'}}>
+        <img src={notfoundimg} alt="" />
+    </Box>
   )
 }
 
